ccc5082f3a0623f7672d022708dd57cc3a7fa1f5
COV <tsehlo@mail.ru>
2022-11-04 16:20:26 +0300

    Списки

--- lists.py
+++ lists.py
@@ -6,6 +6,7 @@ print(array)
 print('array[4] = ', array[4])  # выдаст элемент под индексом 4
 print('длина массива = ', len(array))  # выдаст длину массива
 print('последний элемент в массиве = ', array[-1])  # выдаст последний элемент в массиве
+print('\n')
 
 # массив состоящий из массивов:
 l = [[1, 3, 5], ['a', 'n', 'd'], [1, 6, 9]]
@@ -15,14 +16,15 @@ print('длина массива ', len(l))
 print('обращение к элементу вложенного массива ', l[2][1])
 l[0], l[1] = l[1], l[0]
 print(l)
+print('\n')
 
-
-m = list(input('Введите элементы: '))
+m = list(input('Введите элементы: '))   
 print(m)
-
+print('\n')
 
 n = list(range(10))
 print(n)
+print('\n')
 
 # массив в котором не будет восьмерки
 k = list(range(10))
@@ -35,30 +37,33 @@ else:
     print(s, end=' ')
 print('\n')
 
-
-massiv = [1, 3, 9, 25, 45, 9, 21, 22, 9, 10]
+# методы списка
+massiv = [1, 3, 9, 25, 45, 9, 21, 22, 9, 10]  # объявляем и заполняем массив
 print(massiv)
-massiv.append(23)
+massiv.append(23)   # добавляет элемент в конец списка
 print(massiv)
-massiv.insert(5, 8)
+del massiv[5]
 print(massiv)
-print(massiv.count(9))
-massiv.sort()
+massiv.insert(5, 8) # вставляет элемент по указанному индексу
 print(massiv)
-massiv.reverse()
+print(massiv.count(9))  # позволяет посмотреть сколько одинаковых элементов в списке (9-значение элемента)
+massiv.sort()   # сортирует список по возростанию
 print(massiv)
-y = massiv.pop(3)
+massiv.reverse()    # переварачивает массив в обратную сторону
+print(massiv)
+y = massiv.pop(3)   # удаляет элемент из списка (в скобках индекс удаляемого элемента, 
+                    # если скобки пустые удаляется последний элемент), можно записать удаленный элемент в переменную
 print(y)
 print(massiv)
-massiv.remove(9)
+massiv.remove(9)    # удаляет элемент из списка по его значению (9-значение элемента)
 print(massiv)
-massiv.clear()
+massiv.clear()  # полностью очищает список
 print(massiv)
-massiv.extend([3, 6, 9, 4, 5, 78, 12, 36, 11, 10])
+massiv.extend([3, 6, 9, 4, 5, 78, 12, 36, 11, 10])  # в конец списка добавляет значения из другого списка
 print(massiv)
-newarray = massiv.copy()
+newarray = massiv.copy()    # копирует все данные из другого списка
 print(newarray)
-
+print('\n')
 
 x = list(range(1, 21))
 b = x.copy()
@@ -72,3 +77,25 @@ else:
     print(x)
     print(x1)
 print (b)
+print('\n')
+
+# оператор in позволяет проверить есть элемент в всписе или нет
+city = ['Milan', 'Moscow', 'Paris', 'Rom']
+if 'Moscow' in city:
+    print('есть')
+print('\n')
+
+# Программа winners, интерактивное награждение победителей
+print('Соревнование по Python')
+count = int(input('Введите количество участников: '))
+i = count
+members = []
+while i >0:
+    name = input('Кто занял {} место: '.format(i))
+    members.append(name)
+    i-=1 
+print('В соревновании участвовали: ',sorted(members)) 
+members.reverse()
+result = members[:3]
+print('Победители: {}. Поздравляем!'.format(result)) 
+print('\n')
